4358197ce309d70adc824eb3424d878faf4d259a
AvinSJ <54039321+AvinSJ@users.noreply.github.com>
2020-12-02 21:01:53 +0530

    Add files via upload

--- REST_API.py
+++ REST_API.py
@@ -1,32 +1,47 @@
 from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
 from Db_api import *
+import json
 
 app=Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def index():
-    return "Welcome To The Database"
+index_args=reqparse.RequestParser()
+index_args.add_argument("id", type=int, help="Enter the required id")
+index_args.add_argument("title", type=str, help="Name of the movie")
+index_args.add_argument("director", type=str, help="Director of the movie")
+index_args.add_argument("year", type=int, help="Year of Release")
+index_args.add_argument("genre", type=str, help="Genre of the movie")
 
 
-@app.route("/table", methods=['GET'])
-def get ():
-    return jsonify({'Tables':show_table()})
-
-@app.route("/table", methods=["POST"])
-def create():
-    id=int(input("Add a new ID :"))
-    title=str(input("TITLE :"))
-    director=str(input("DIRECTOR :"))
-    year=int(input("YEAR :"))
-    genre=str(input("GENRE :"))
-    data=insert(id,title,director,year,genre)
-    return data
-
-create()
+table=show_table()
 
 
+out_file = open("moviedata.json", "w")
+table=json.dump(table, out_file)
+out_file.close()
 
+f = open('myfile.json', )
+data = json.load(f)
+f.close()
+
+d={}
+
+class Table(Resource):
+	def get(self,id):
+		return data
+	def put(self, id):
+		args=index_args.parse_args()
+		d[id]=args
+		return print(d[id])
+
+
+
+
+api.add_resource(Table, "/<int:id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
